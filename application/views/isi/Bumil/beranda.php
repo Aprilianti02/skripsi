beccc98bbd5fb9e750139980dc1294da820f4b82
Aprilianti02 <77833562+Aprilianti02@users.noreply.github.com>
2021-03-02 23:19:08 +0700

    push

--- application/views/isi/Bumil/beranda.php
+++ application/views/isi/Bumil/beranda.php
@@ -6,7 +6,7 @@
         <br>
 		<p class="mb-4">Anemia adalah suatu keadaan dimana tubuh memiliki jumlah sel darah merah yaitu eritrosit yang
 			terlalu sedikit,sel darah merah itu mengandung hemoglobin yang berfungsi untuk membawa oksigen ke seluruh
-			jaringan tubuh<a target="_blank" href="https://beranda.net"></a>.</p>
+			jaringan tubuh.Anemia pada kehamilan adalah kurangan zat besi yang terjadi ketika tubuh kekurangan sel darah merah yang berfungsi menyebarkan oksigen ke seluruh tubuh. Kasus anemia yang paling sering terjadi adalah anemia defisiensi besi. Anemia  defisiensi besi  pada wanita hamil merupakan problema kesehatan yang dialami oleh wanita diseluruh dunia terutama dinegara berkembang.  <a target="_blank" href="https://beranda.net"></a>.</p>
 	</center>
 
 </div>
--- application/views/isi/Bumil/informasi.php
+++ application/views/isi/Bumil/informasi.php
@@ -3,7 +3,7 @@
 
                     <!-- Page Heading -->
                     <h1 class="h3 mb-2 text-gray-800">Informasi Sistem Pakar</h1>
-                    <p class="mb-4">Sistem Pakar yaitu suatu program komputer yang dirancang untuk mengambil keputusan seperti keputusan yang diambil oleh seseorang atau beberapa orang pakar <a target="_blank"
+                    <p class="mb-4">Sistem Pakar yaitu suatu program komputer yang dirancang untuk mengambil keputusan seperti keputusan yang diambil oleh seseorang atau beberapa orang pakar, Keuntungan dari adanya sistem pakar ini adalah orang yang bukan ahli pada bidangnya ataupun masyarakat umum, dapat menyelesaikan atau memecahkan suatu masalah sama seperti para ahli.  <a target="_blank"
                             href="https://beranda.net"></a>.</p>
                 </div>
           
