72130abb64f7410a9b8ff569327bd9489098f066
Yusril Fahmi Al Faizi <44188722+yusrilfahmialfaizi@users.noreply.github.com>
2021-03-02 23:26:06 +0700

    Update anemia.sql

--- db/anemia.sql
+++ db/anemia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 02, 2021 at 02:54 PM
+-- Generation Time: Mar 02, 2021 at 05:25 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.10
 
