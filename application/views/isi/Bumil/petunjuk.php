dd0b02dd193988c82d743baa4714ca006b76e83d
Aprilianti02 <77833562+Aprilianti02@users.noreply.github.com>
2021-06-29 19:22:55 +0700

    hihhu

--- application/views/isi/Bumil/petunjuk.php
+++ application/views/isi/Bumil/petunjuk.php
@@ -3,7 +3,7 @@
 
                     <!-- Page Heading -->
                     <h1 class="h3 mb-2 text-gray-800">Panduan</h1>
-                    <p class="mb-4">User : Sebelum login diharuskan memiliki username dan password  <a target="_blank"
+                    <p class="mb-4">User : Sebelum login diharuskan memiliki username dan password   <a target="_blank"
                             href="https://beranda.net"></a>.</p>
                 </div>
           
